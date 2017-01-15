dd42846c1b8ce226ba541ee61f2e723ebc640d0a
David Trethewey <davidtreth@gmail.com>
2017-01-15 02:49:34 +0000

    rewrite to make it more functional, with reading data and printing it separated. do checks to see if the name of the ship and its registry number is the same throughout each series

--- abership.py
+++ abership.py
@@ -38,147 +38,233 @@ from openpyxl import load_workbook
 from operator import itemgetter
 from collections import defaultdict
 
-shipdict = defaultdict(dict)
+def getVesselsInfo(verbose=False):
+    if verbose:
+        print("Yn ddarllen y data\nReading Data")
+    shipdict = defaultdict(dict)
 
-transcriptdir = glob.glob("ABERSHIP_transcription*")
+    transcriptdir = glob.glob("ABERSHIP_transcription*")
 
-if len(transcriptdir) > 1:
-    print("only one ABERSHIP_transcription directory expected")
-    print(transcriptdir)
-else:
-    transcriptdir = transcriptdir[0]
-    os.chdir(transcriptdir)
+    if len(transcriptdir) > 1:
+        print("only one ABERSHIP_transcription directory expected")
+        print(transcriptdir)
+    else:
+        transcriptdir = transcriptdir[0]
+        os.chdir(transcriptdir)
     
-seriesdirs = glob.glob("Series*")
-# make the directories be sorted by number not alphabetically
-# i.e. 11 should come before 101 etc.
-seriesstarts = [int(d.split(" ")[1]) for d in seriesdirs]
-#print(seriesdirs)
-#print(seriesstarts)
-seriesdirssorted = list(zip(seriesdirs, seriesstarts))
-seriesdirssorted.sort(key=itemgetter(1))
-#print(seriesdirssorted)
+    seriesdirs = glob.glob("Series*")
+    # make the directories be sorted by number not alphabetically
+    # i.e. 11 should come before 101 etc.
+    seriesstarts = [int(d.split(" ")[1]) for d in seriesdirs]
+    #print(seriesdirs)
+    #print(seriesstarts)
+    seriesdirssorted = list(zip(seriesdirs, seriesstarts))
+    seriesdirssorted.sort(key=itemgetter(1))
+    #print(seriesdirssorted)
 
-# count how many Excel files we have
-totalNfiles = 0
+    # count how many Excel files we have
+    totalNfiles = 0
 
-for s in seriesdirssorted:
-    # loop through the top-level directies
-    print("\n")
-    print(s[0])
-    os.chdir(s[0])
-    # find the second level directories (1 per ship) and sort by number
-    filedirs = glob.glob("Series*")
-    # the  [:3] is needed for Series_525a_vtls004574161
-    fileindices = [int(d.split("_")[1][:3]) for d in filedirs]
-    filedirssorted = list(zip(filedirs, fileindices))
-    filedirssorted.sort(key=itemgetter(1))
-    #print(filedirssorted)
-    for f in filedirssorted:
-        # loop through the second level directories
-        print("\n")
-        print(f[0])
-        os.chdir(f[0])
-        shipdict[f[1]] = defaultdict(dict)
-        shipdict[f[1]]["Vessel Names2"] = []
-        # find the Excel files, and sort by number
-        filelist = glob.glob("File*.xlsx")  
-        totalNfiles += len(filelist)
-        filelistns = [int(x.split("_")[1].split("-")[1]) for x in filelist]
-        filelistsorted = list(zip(filelist, filelistns))
-        filelistsorted.sort(key=itemgetter(1))
-        for i in filelistsorted:
-            # loop through the Excel files
-            print("\n")
-            print(i[0])
-            shipdict[f[1]][i[1]] = defaultdict(dict)
-            wb = load_workbook(filename=i[0])
-            sheets = wb.get_sheet_names()
-            for sheet in sheets:
-                # loop through the sheets in each workbook
-                print("Sheet: {sh}".format(sh=sheet))
-                sheet_ranges = wb[sheet]
-                # cell F2 contains vessel name, F4 its number and F6 the port of registry
-                shipname = sheet_ranges['F2'].value
-                # assume the vessel name is the same for each series
-                # should be, I think.
-                # take the first one it finds for each series
-                try:
-                    assert(len(shipdict[f[1]]['Vessel Name'])>0)
-                except:
-                    shipdict[f[1]]["Vessel Name"] = shipname
-                # add to list of all ship names in each series
-                shipdict[f[1]]["Vessel Names2"].append(shipname)
-                shipnum = sheet_ranges['F4'].value
-                shipport = sheet_ranges['F6'].value
-                print("Ship name: {n}. Official Number: {n2} Port of registry: {p}\n".format(n=shipname, n2=shipnum, p=shipport))
-                # headings are on row 8, 2 rows with example data 10-11
-                rownum = 12
-                # could put anything here, so that while loop doesn't immediately stop
-                marinername = "Matthew Trewella"
-                print("{n:30}\t{y:10} {a:8}\t{p:20}\t{d:11} {pj:20} {c:20}\t{d2:20} {pl}".format(n="Name",
-                                                                                                y="Birth Year",
-                                                                                                a="Age",
-                                                                                                p="Birthplace",
-                                                                                                d="Date Joined",
-                                                                                                pj="Port Joined",
-                                                                                                c="Capacity",
-                                                                                                d2="Date Left",
-                                                                                                pl="Port Left"))
-                while marinername:                    
-                    marinername = sheet_ranges['A{r}'.format(r=rownum)].value
-                    marinerbyear= sheet_ranges['B{r}'.format(r=rownum)].value
-                    if type(marinerbyear) is datetime.datetime:
-                        marinerbyear = marinerbyear.date()
-                    marinerage= sheet_ranges['C{r}'.format(r=rownum)].value
-                    marinerbplace= sheet_ranges['D{r}'.format(r=rownum)].value
-                    marinerdatejoin = sheet_ranges['J{r}'.format(r=rownum)].value
-                    if type(marinerdatejoin) is datetime.datetime:
-                        marinerdatejoin = marinerdatejoin.date()
-                    marinerportjoin = sheet_ranges['K{r}'.format(r=rownum)].value
-                    marinercap= sheet_ranges['L{r}'.format(r=rownum)].value
-                    marinerdateleft = sheet_ranges['M{r}'.format(r=rownum)].value
-                    if type(marinerdateleft) is datetime.datetime:
-                        marinerdateleft = marinerdateleft.date()
-                    marinerportleft = sheet_ranges['N{r}'.format(r=rownum)].value                   
-                    if marinername:
-                        print("{n:30}\t{y:10} {a:8}\t{p:20}\t{d:11} {pj:20} {c:20}\t{d2:20} {pl}".format(n=marinername,
-                                                                                                           y=str(marinerbyear),
-                                                                                                           a=str(marinerage),
-                                                                                                           p=str(marinerbplace),
-                                                                                                           d=str(marinerdatejoin),
-                                                                                                           pj=str(marinerportjoin),
-                                                                                                           c=str(marinercap),
-                                                                                                           d2=str(marinerdateleft),
-                                                                                                           pl=str(marinerportleft)))
-                    rownum += 1
+    for s in seriesdirssorted:
+        # loop through the top-level directies
+        #print("\n")
+        if verbose:
+            print("Directory: {d}".format(d=s[0]))
+        os.chdir(s[0])
+        # find the second level directories (1 per ship) and sort by number
+        filedirs = glob.glob("Series*")
+        # the  [:3] is needed for Series_525a_vtls004574161
+        fileindices = [int(d.split("_")[1][:3]) for d in filedirs]
+        filedirssorted = list(zip(filedirs, fileindices))
+        filedirssorted.sort(key=itemgetter(1))
+        #print(filedirssorted)
+        for f in filedirssorted:
+            # loop through the second level directories
+            #print("\n")
+            if verbose:
+                print("Directory: {d}".format(d=f[0]))
+            os.chdir(f[0])
+            shipdict[f[1]] = defaultdict(dict)
+            shipdict[f[1]]["Vessel Names2"] = []
+            shipdict[f[1]]["VesselIDs"] = []
+            # find the Excel files, and sort by number
+            filelist = glob.glob("File*.xlsx")  
+            totalNfiles += len(filelist)
+            filelistns = [int(x.split("_")[1].split("-")[1]) for x in filelist]
+            filelistsorted = list(zip(filelist, filelistns))
+            filelistsorted.sort(key=itemgetter(1))
+            for i in filelistsorted:
+                # loop through the Excel files
+                #print("\n")
+                if verbose:
+                    print("File: {fn}".format(fn=i[0]))
+                shipdict[f[1]][i[1]] = defaultdict(dict)                
+                wb = load_workbook(filename=i[0])
+                sheets = wb.get_sheet_names()
+                for sheet in sheets:
+                    # loop through the sheets in each workbook
+                    if verbose:                        
+                        print("Sheet: {sh}".format(sh=sheet))
+                    sheet_ranges = wb[sheet]
+                    # cell F2 contains vessel name, F4 its number and F6 the port of registry
+                    shipname = sheet_ranges['F2'].value
+
+                    shipnum = sheet_ranges['F4'].value                
+                    shipport = sheet_ranges['F6'].value
+                    # assume the vessel name is the same for each series
+                    # should be, I think, except maybe if a vessel changes name
+                    # take the first one it finds for each series
+                    try:
+                        assert(len(shipdict[f[1]]['Vessel Name'])>0)
+                    except:
+                        shipdict[f[1]]["Vessel Name"] = shipname
+                        shipdict[f[1]]["VesselID"] = int(shipnum)
+                    try:
+                        assert(len(shipdict[f[1]]["Port"])>0)
+                    except:
+                        shipdict[f[1]]["Port"] = shipport
+                    # add to list of all ship names in each series
+                    shipdict[f[1]]["Vessel Names2"].append(shipname)
+                    if shipnum:
+                        try:
+                            shipnum = int(shipnum)
+                        except:
+                            pass
+                        shipdict[f[1]]["VesselIDs"].append(shipnum)
+                        
+                    shipdict[f[1]][i[1]][sheet]["Crewlist"] = []
+                    # headings are on row 8, 2 rows with example data 10-11
+                    rownum = 12
+                    # could put anything here, so that while loop doesn't immediately stop
+                    marinername = "Matthew Trewella"
+                    
+
+                    while marinername:                    
+                        marinername = sheet_ranges['A{r}'.format(r=rownum)].value
+                        marinerbyear= sheet_ranges['B{r}'.format(r=rownum)].value
+                        if type(marinerbyear) is datetime.datetime:
+                            marinerbyear = marinerbyear.date()
+                        marinerage= sheet_ranges['C{r}'.format(r=rownum)].value
+                        marinerbplace= sheet_ranges['D{r}'.format(r=rownum)].value
+                        marinerdatejoin = sheet_ranges['J{r}'.format(r=rownum)].value
+                        if type(marinerdatejoin) is datetime.datetime:
+                            marinerdatejoin = marinerdatejoin.date()
+                        marinerportjoin = sheet_ranges['K{r}'.format(r=rownum)].value
+                        marinercap= sheet_ranges['L{r}'.format(r=rownum)].value
+                        marinerdateleft = sheet_ranges['M{r}'.format(r=rownum)].value
+                        if type(marinerdateleft) is datetime.datetime:
+                            marinerdateleft = marinerdateleft.date()
+                        marinerportleft = sheet_ranges['N{r}'.format(r=rownum)].value                   
+                        if marinername:
+                            crewdict = {"name":marinername, "byear":str(marinerbyear), "age":str(marinerage),
+                                        "bplace":str(marinerbplace), "datejoin":str(marinerdatejoin),
+                                        "portjoin":str(marinerportjoin), "capacity":str(marinercap),
+                                        "dateleft":str(marinerdateleft), "portleft":str(marinerportleft)}
+
+                                                                                                     
+                            shipdict[f[1]][i[1]][sheet]["Crewlist"].append(crewdict)
+                        rownum += 1
                         
                     
             #print("Sheets: {sh}".format(sh=sheets))
+            os.chdir("..")
         os.chdir("..")
-    os.chdir("..")
-#print(shipdict)    
-shipnames = [shipdict[s]["Vessel Name"] for s in shipdict]
-#shipnames.sort()
+    print("\nTotal number of Excel files = {t}".format(t=totalNfiles))
+    return shipdict
+#print(shipdict) 
+    
+def printCrewLists(shipdict):
+    shipnames = [shipdict[s]["Vessel Name"] for s in shipdict]
+    #shipnames.sort()
+    
+    print("Number of vessels = {n}".format(n=len(shipnames)))
+    #print(shipdict)
+    for s in shipdict:
+        for i in shipdict[s]:
+            #print(i)
+            if not(type(i) is int):
+                continue
+            for ws in shipdict[s][i]:
+                
+                
+                #print(shipdict[s][i])
 
-print("Number of vessels = {n}".format(n=len(shipnames)))
-print("\nVessel names: ")
-#for s in shipnames:
-#    print(s, end=", ")
+                #print(s, i, ws)
+                #print(shipdict[s][i][ws])
 
-# now checks whether all instances of the ship name
-# are the same as that of the first one
-shipnames2 = [shipdict[s]["Vessel Names2"] for s in shipdict]
-zshipnames = list(zip(shipnames, shipnames2))
-for z in zshipnames:
-    print(z[0])
-    for s2 in z[1]:
+                crewlist = shipdict[s][i][ws]["Crewlist"]
+                if len(crewlist) > 0:
+                    print("Ship name: {n}. Ship Registry Number: {n2} Port of registry: {p}\n".format(
+                    n=shipdict[s]["Vessel Name"], n2=shipdict[s]["VesselID"], p=shipdict[s]["Port"]))
+                    print("{n:30}\t{y:10} {a:8}\t{p:20}\t{d:11} {pj:20} {c:20}\t{d2:20} {pl}".format(n="Name",
+                          y="Birth Year",
+                          a="Age",
+                          p="Birthplace",
+                          d="Date Joined",
+                          pj="Port Joined",
+                          c="Capacity",
+                          d2="Date Left",
+                          pl="Port Left"))
+                      
+                for mar in crewlist:
+                    #print(mar)
+                    print("{n:30}\t{y:10} {a:8}\t{p:20}\t{d:11} {pj:20} {c:20}\t{d2:20} {pl}".format(n=mar["name"],
+                          y=mar["byear"], a=mar["age"], p=mar["bplace"], d=mar["datejoin"], pj=mar["portjoin"],
+c=mar["capacity"], d2=mar["dateleft"], pl=mar["portleft"]))
+                print("\n")
+        print("\n")
+
+def checkNames(shipdict):
+    shipnames = [shipdict[s]["Vessel Name"] for s in shipdict]
+    #shipnames.sort()
+    
+    print("Number of vessels = {n}".format(n=len(shipnames)))
+    print("\nVessel names: ")
+    for s in shipnames:
+        print(s, end=", ")
+    print("\n")  
+    # now checks whether all instances of the ship name
+    # are the same as that of the first one
+    shipnames2 = [shipdict[s]["Vessel Names2"] for s in shipdict]
+    shipnumbers = [shipdict[s]["VesselIDs"] for s in shipdict]
+    zshipnames = list(zip(shipnames, shipnames2, shipnumbers))
+    for z in zshipnames:
+        print("{n} : {w} worksheets".format(n=z[0], w=len(z[1])))
         try:
-            assert(z[0] == s2)
-        except:
-            print("ship name conflict")
-            print(z[0], s2)
-            print("\n")
-            #time.sleep(5)    
+            assert len(list(set(z[2]))) == 1
+        except:              
+            print("more than one ship registry number per series")
+            print(z[2])
+    
+        for s2 in z[1]:
+            try:
+                assert(z[0].strip() == s2.strip())
+            except:
+                print("ship name conflict")
+                print(z[0], s2)
+                #time.sleep(5)
+  
 
-print("\nTotal number of Excel files = {t}".format(t=totalNfiles))
+
+
+if __name__ == '__main__':
+    """
+    If invoked at the command-line
+    """
+    # Create the command line options parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--crewlists", action="store_true",
+                        help="print all crew lists to console")
+    parser.add_argument("-c", "--checkships", action="store_true",
+                        help="check whether the ship names and registry number match across each 'series'")
+    parser.add_argument("-v", "--verbose", action="store_true",
+                        help="verbose mode - print statements while reading data")
+    args = parser.parse_args()
+    
+    shipdict = getVesselsInfo(verbose=args.verbose)
+    if args.crewlists:
+        printCrewLists(shipdict)
+    if args.checkships:
+        checkNames(shipdict)
+                    
+    
